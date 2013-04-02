1b3d14c2029312d23dcd155b5a27879591bf7d4d
unknown <bslater@CSCLASS-13.cs.hartford.edu>
2013-04-02 16:12:33 -0400

    skeleton code

---
+++ src/videorentalstore/movies/MovieDatabase.java
@@ -0,0 +1,20 @@
+package videorentalstore.movies;
+
+/**
+ * Created 4/2/13
+ * Last Edited 4/2/13
+ * @author bslater
+ */
+public class MovieDatabase {
+    public void addMovieToDB(Movie movie) {
+        //add movie into database
+    }
+    
+    public void remMovieFromDB(Movie movie) {
+        //find movie in database, remove it
+    }
+    
+    public void editMovieInDB(Movie movie) {
+        //find movie in database, compare "changes", if different, make changes
+    }
+}
---
+++ src/videorentalstore/movies/MovieListSort.java
@@ -0,0 +1,10 @@
+package videorentalstore.movies;
+
+/**
+ * Created 4/2/13
+ * Last Edited 4/2/13
+ * @author bslater
+ */
+public class MovieListSort {
+    
+}
